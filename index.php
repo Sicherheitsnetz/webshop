ca11d383ce4f3d3d6ff1edc27181d48418ec3df0
Andreas Kleiner <andreas.kleiner@outlook.de>
2021-09-17 11:43:44 +0000

    index.php

--- index.php
+++ index.php
@@ -26,7 +26,8 @@ function query($query) {
 
 <!-- just another comment -->
 <!-- and one more -->
-
+<!-- third comment -->
+<!-- fouth comment -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
