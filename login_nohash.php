753a39818e595036b1aaba5402705db1fe15bc21
Andreas Kleiner <andreas.kleiner@outlook.de>
2021-09-17 13:16:37 +0000

    login_nohash.php

--- login_nohash.php
+++ login_nohash.php
@@ -38,6 +38,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
+	    echo $stmt;		
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
@@ -52,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
