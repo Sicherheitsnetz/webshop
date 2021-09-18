aeacf425aeb0a1fe3434c0d3c3793bed567144ff
Andreas Kleiner <andreas.kleiner@outlook.de>
2021-09-18 20:08:07 +0000

    config.php

--- config.php
+++ config.php
@@ -13,4 +13,45 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+function template_header($title) {
+	echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+	</head>
+	<body>
+        <header>
+            <div class="content-wrapper">
+                <h1>Shopping Cart System</h1>
+                <nav>
+                    <a href="index.php">Home</a>
+                    <a href="index.php?page=products">Products</a>
+                </nav>
+                <div class="link-icons">
+                    <a href="index.php?page=cart">
+						<i class="fas fa-shopping-cart"></i>
+					</a>
+                </div>
+            </div>
+        </header>
+        <main>
+EOT;
+}
+function template_footer() {
+$year = date('Y');
+echo <<<EOT
+        </main>
+        <footer>
+            <div class="content-wrapper">
+                <p>&copy; $year, Shopping Cart System</p>
+            </div>
+        </footer>
+        <script src="script.js"></script>
+     </body>
+</html>
+EOT;
+}
 ?>
+
